f0759e0b5b212ec56c9d16e687fc03d527ebf00d
Phillip Sipe <phillip.sipe@makethunder.com>
2017-12-21 17:34:34 -0600

    test a few things out

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,13 +12,35 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import styled from 'styled-components';
+import Tone from 'tone';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const Title = styled.h1`
+      font-size: 1.5em;
+      text-align: center;
+      color: #4A148C;
+    `
+    var Synth = Tone.PolySynth;
+    var synth = new Synth().toMaster();
+    var loop = new Tone.Loop(time => {
+
+      synth.triggerAttackRelease(["C4", "E4", "G4", "Bb4"], "4n");
+    },"5n")
+
+    loop.start("1m").stop("5m");
+    Tone.Transport.bpm.value = 120
+    Tone.Transport.start();
+
+
+
+    console.log(synth)
+
     return (
-      <h1>
+      <Title>
         <FormattedMessage {...messages.header} />
-      </h1>
+      </Title>
     );
   }
 }
